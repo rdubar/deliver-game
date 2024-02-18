ee457f526f94864cffb5dd16435a9b374ef2f2aa
Roger Dubar <rdubar@gmail.com>
2024-02-18 09:28:44 +0000

    load_data fix

--- utils.py
+++ utils.py
@@ -1,15 +1,16 @@
-# Utilities 
 import os
 
-def load_data(file, split=False):
-    if not os.path.exists(file):
-        file = os.path.join('data', file)
+def load_data(filename, split=False):
+    current_script_dir = os.path.dirname(__file__)
+    data_dir = os.path.join(current_script_dir, 'data')
+    file_path = os.path.join(data_dir, filename)
+    
     try:
-        with open(file, 'r') as f:
+        with open(file_path, 'r') as f:
             data = f.read()
         if split:
             data = data.split('\n')
         return data
     except Exception as e:
-        print(e)
+        print(f"Error loading data from {file_path}: {e}")
         return None
