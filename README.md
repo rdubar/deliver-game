3f270f9a1d69bfa3d4326060a3c5660e92e40b5d
Roger Dubar <rdubar@gmail.com>
2024-03-07 22:17:08 +0000

    Update GitHub Copilot credit

--- README.md
+++ README.md
@@ -89,5 +89,5 @@ Distributed under the MIT License. See LICENSE for more information.
 * Alphapet Ventures - My employer's parent company, for their support.
 * Streamlit - "The fastest way to build and share data apps."
 * Python - The programming language used to create this app.
-* OpenAI - Powering ChatGPT & Microsoft Co-Pilot, for rapid development.
+* OpenAI - Powering ChatGPT & GitHub Copilot, for rapid development.
 * MongoDB - Very simple database management.
--- pages/6_Thanks.py
+++ pages/6_Thanks.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Delivery Game", page_icon=":game_die:")
 * Alphapet Ventures - My employer's parent company, for their support.
 * Streamlit - "The fastest way to build and share data apps."
 * Python - The programming language used to create this app.
-* OpenAI - Powering ChatGPT & Microsoft Co-Pilot, for rapid development.
+* OpenAI - Powering ChatGPT & GitHub Copilot, for rapid development.
 * MongoDB - Very simple database management.
 """
 
